82196e72d02c8d57eb784d54e5323786f44f716b
Guymangh <guymangh@gmail.com>
2026-05-27 19:08:52 -0700

    UI: Integrated embedded MarineTraffic map and voyage timeline directly into Logistics Hub search

---
+++ cubag-backend/static/assets/esm-D4lkYY5k.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DrjwQedp.js","assets/dist-CQS6rvaI.js"])))=>i.map(i=>d[i]);
+import{r as e}from"./dist-CQS6rvaI.js";import{d as t}from"./index-DyoimCbP.js";var n=e(`Browser`,{web:()=>t(()=>import(`./web-DrjwQedp.js`).then(e=>new e.BrowserWeb),__vite__mapDeps([0,1]))});export{n as t};
