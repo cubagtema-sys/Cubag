9c36dd13b492bb09e5291361752abef7c4aa8481
Guymangh <guymangh@gmail.com>
2026-06-05 10:20:07 -0700

    feat: add google-services.json and fix applicationId to com.cubag_flutter for Firebase FCM

--- cubag_flutter/android/app/build.gradle.kts
+++ cubag_flutter/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cubag_flutter"
+    namespace = "com.cubag_flutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,8 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cubag_flutter"
+        applicationId = "com.cubag_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
