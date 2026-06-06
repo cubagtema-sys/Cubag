5cb17c5346c2d603a8f303eaaab22c0135e97b5c
Guymangh <guymangh@gmail.com>
2026-06-06 05:50:53 -0700

    chore: deploy updated flutter web build with correct production websocket config

--- cubag-backend/static/flutter_bootstrap.js
+++ cubag-backend/static/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1199549727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1795264059" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
