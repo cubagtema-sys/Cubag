ab46750c3686b68489fcdf79a48fec32fc0b0e33
crypto <CryptoPlexTrade@hotmail.com>
2026-06-09 23:00:47 +0200

    fix: Remove duplicate old bottom navigation bar from UserShell

--- cubag-backend/dist/flutter_bootstrap.js
+++ cubag-backend/dist/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3752322462" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "53552792" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
